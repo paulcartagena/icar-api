0d42ff87415f30e7579acbe59588c56fb6dd72d9
paulcartagena <paulcartagena04@gmail.com>
2026-09-03 00:12:18 -0600

    initial commit: Spring Boot project setup

---
+++ src/main/java/com/paulcartagena/icarapi/IcarApiApplication.java
@@ -0,0 +1,13 @@
+package com.paulcartagena.icarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcarApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcarApiApplication.class, args);
+    }
+
+}
